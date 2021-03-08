830c261f41f60702f16a1bb69a6a575589e3a10a
TheDummi <73711717+TheDummi@users.noreply.github.com>
2021-03-08 16:26:25 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -10,7 +10,7 @@ const contributors = [
 class InfoCommand extends Command {
 	constructor() {
 		super('Info', {
-			aliases: ['info', 'i', 'botinfo'],
+			aliases: ['info', 'i', 'botinfo', 'developer', 'contributors', 'servers', 'library', 'version', 'uptime', 'users', ],
 			category: 'info',
 			description: 'Get general info about the bot.',
 			channel: ['guild', 'dm']
@@ -18,10 +18,15 @@ class InfoCommand extends Command {
 	}
 
 	async exec(message, args) {
+                Let embed = new Discord.MessageEmbed()
+                   .setColor(0xaa00cc)
+                If (message.util.parsed.alias === 'developer) {
+                
+
 		let uptime = getUptime(this.client).uptime;
 		const owners = (await Promise.all(this.client.ownerID.map(id => this.client.users.fetch(id)))).map(u => u.tag)
 		let m = await message.util.send('calculating...')
-		let embed = new Discord.MessageEmbed()
+		embed = embed
 			.setTitle(`Bot Info`)
 			.addField(`| Bot makers`, owners, true)
 			.addField(`| Contributors`, contributors, true)
@@ -32,7 +37,6 @@ class InfoCommand extends Command {
 			.addField(`| Version`, `\`\`\`glsl\nV${version}\`\`\``, true)
 			.addField(`| Git repository`, '[DummiBot v2](https://github.com/TheDummi/DummiBot-v2.0.git)', true)
 			.addField(`| Uptime`, `\`\`\`glsl\n${uptime}\`\`\``)
-			.setColor(0xaa00cc) 
 			.setFooter('This message gets deleted after 2 minutes.')
 		await m.edit("",embed)
 		.then(message => {
